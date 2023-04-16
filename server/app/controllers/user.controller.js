888bd27123ed04693c0f297c7f03e1d3a11df7c2
proudhell <proudhell@gmail.com>
2023-04-16 22:58:31 +0300

    update api

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -11,28 +11,22 @@ exports.all = (req, res) => {
       res.send(products);
     }).catch(err => console.log(err));
   };
-exports.brand = (req, res) => {
-  Product.findAll({
-    where: {
-      brand: req.body.brand,
-    },include: [{
-      model: Compound
-    }],
-  }).then(products => {
-    res.send(products);
-  }).catch(err => console.log(err));
-};
-exports.type = (req, res) => {
-  Product.findAll({
-    where: {
-      type: req.body.type,
-    },include: [{
-      model: Compound
-    }],
-  }).then(products => {
-    res.send(products);
-  }).catch(err => console.log(err));
-};
+  exports.brand = (req, res) => {
+    Product.findAll({
+      attributes: ['brand'],
+      group: ['brand']
+    }).then(products => {
+      res.send(products);
+    }).catch(err => console.log(err));
+  };
+  exports.type = (req, res) => {
+    Product.findAll({
+      attributes: ['type'],
+      group: ['type']
+    }).then(products => {
+      res.send(products);
+    }).catch(err => console.log(err));
+  };
 exports.name = (req, res) => {
   Product.findAll({
     where: {
